640b93199802a742aabdaae1737e1ed9ee8e6e57
WebPopStar <aurora980331@gmail.com>
2024-09-06 18:17:48 -0500

    footer

--- src/pages/dashBoard/DashLayout.jsx
+++ src/pages/dashBoard/DashLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/dashBoard/header/Index";
-import Footer from "../../components/dashBoard/footer";
+import Footer from "../../components/dashBoard/footer/Index";
 import { Outlet } from "react-router";
 
 const DashLayout = () => {
